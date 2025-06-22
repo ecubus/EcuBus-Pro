672df7b5ca97464391d2de318b788d1fafe1b6ce
Frankie Zeng <frankie.zengfu@gmail.com>
2025-06-22 11:31:12 +0800

    update demo

--- resources/examples/doip_gateway/node.ts
+++ resources/examples/doip_gateway/node.ts
@@ -3,7 +3,7 @@ import { DiagResponse, output } from 'ECB'
 Util.On('Tester_eth_1.*.send', async (req) => {
   console.log('Received DOIP Diag request', req.getUdsAddress().ethAddr?.name)
   req.testerName = 'Tester_can_0'
-  await req.outputDiag('PEAK_1', req.getUdsAddress().ethAddr?.name)
+  await req.outputDiag('SIMULATE_1', req.getUdsAddress().ethAddr?.name)
 })
 Util.On('Tester_can_0.*.recv', async (resp) => {
   console.log('Received CANTP Diag response', resp.getUdsAddress().canAddr?.name)

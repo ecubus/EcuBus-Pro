a30d2ccdd9643c9b5f81560c6b737827e08bfb68
frankie zeng <frankie.zengfu@gmail.com>
2025-08-20 10:56:12 +0800

    [bug]:fix wepback rewrite require to xxxx

--- cli/webpack.config.js
+++ cli/webpack.config.js
@@ -8,9 +8,14 @@ class PreprocessEcbCliPlugin {
         compiler.hooks.afterEmit.tapAsync('PreprocessEcbCliPlugin', (compilation, callback) => {
             const ecbCliPath = path.resolve(__dirname, 'dist', 'ecb_cli.js');
             const content = fs.readFileSync(ecbCliPath, 'utf8');
-            const processedContent = content.replace(
+            let processedContent = content.replace(
                 /path\.join\(__dirname\s*,\s*([^)]*)\)\.replace\(\s*["']app\.asar["']\s*,\s*["']app\.asar\.unpacked["']\s*\)/g,
                 'path.join(path.dirname(process.execPath), $1)'
+            );
+            //require: __webpack_require__("./out sync recursive"), to require:require
+            processedContent = processedContent.replace(
+                /require:\s*__webpack_require__\s*\(\s*["']\.\/out\s+sync\s+recursive["']\s*\)/g,
+                'require:require'
             );
             fs.writeFileSync(ecbCliPath, processedContent, 'utf8');
             callback();
@@ -39,7 +44,6 @@ module.exports = {
             '@serialport/bindings-cpp': path.resolve(__dirname, '../resources/lib/js/bindings-cpp.js'),
             bindings: path.resolve(__dirname, '../resources/lib/js/node-bindings.js'),
         },
-
     },
     target: 'node',
     module: {
